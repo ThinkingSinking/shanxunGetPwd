87dead26f1f70f5fea07ccba90027aab1c92e352
ThinkingSinking <ThinkingSinking@yahoo.com>
2015-12-27 14:28:31 +0800

    简化了界面
    界面有两部分组成，信息提示框和获取按钮

--- ShanxunGetPwd/app/src/main/java/com/tools/zjd/shanxungetpwd/MainActivity.java
+++ ShanxunGetPwd/app/src/main/java/com/tools/zjd/shanxungetpwd/MainActivity.java
@@ -12,8 +12,8 @@ import android.widget.Toast;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView msg;
-    private Button submit;
+    private TextView msg;   // 状态信息框
+    private Button submit;  //获取按钮
     private boolean state=true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,23 +26,27 @@ public class MainActivity extends AppCompatActivity {
     class submit_Listener implements OnClickListener {
         public void onClick(View v){
             try {
-                msg.setText("正在获取闪讯密码，请稍等......");
-                state=false;
-                String phoneNumber="1065930051";
-                String message="MM";
-                // 获取短信管理器
-                SmsManager smsManager = SmsManager.getDefault();
-                // 拆分短信内容（手机短信长度限制）
-                List<String> divideContents = smsManager.divideMessage(message);
-                for (String text : divideContents) {
-                    smsManager.sendTextMessage(phoneNumber, null, text, null, null);
+                if(state){
+                    msg.setText("正在获取闪讯密码，请稍等......");
+                    state=false;
+                    String phoneNumber="1065930051";
+                    String message="MM";
+                    // 获取短信管理器
+                    SmsManager smsManager = SmsManager.getDefault();
+                    // 拆分短信内容（手机短信长度限制）
+                    List<String> divideContents = smsManager.divideMessage(message);
+                    for (String text : divideContents) {
+                        smsManager.sendTextMessage(phoneNumber, null, text, null, null);
+                    }
+                    //Toast.makeText(MainActivity.this, "发送成功", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "正在获取中，请稍等...", Toast.LENGTH_LONG).show();
                 }
-                Toast.makeText(MainActivity.this, "发送成功", Toast.LENGTH_LONG).show();
             }catch (Exception e){
                 Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
             }
-
         }
     }
+
 
 }
