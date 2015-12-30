3e9181f636b5b2fcf4cc28c911b2236933cc1044
ThinkingSinking <ThinkingSinking@yahoo.com>
2015-12-30 21:24:56 +0800

    监听数据库拦截短信
    将原来的短信广播监听改为了短信数据库的监听，并将数据库中收到的短信删除，达到拦截的目的。

--- ShanxunGetPwd/app/src/main/java/com/tools/zjd/shanxungetpwd/MainActivity.java
+++ ShanxunGetPwd/app/src/main/java/com/tools/zjd/shanxungetpwd/MainActivity.java
@@ -1,14 +1,14 @@
 
 package com.tools.zjd.shanxungetpwd;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
+import android.content.ContentResolver;
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
-import android.telephony.SmsMessage;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -20,9 +20,10 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private TextView msg;   // 状态信息框
     private Button submit;  //获取按钮
+    private int i=0;
     //0表示初始状态,1表示短信已发送但还没收到回信,2表示短信接收成功
     private int state=0;
-    private MySMSReceiver mySMSReceiver;
+
     protected void onCreate(Bundle savedInstanceState) {
         try{
             super.onCreate(savedInstanceState);
@@ -32,11 +33,11 @@ public class MainActivity extends AppCompatActivity {
             this.submit=(Button)findViewById(R.id.submit);//获取按钮
             this.submit.setOnClickListener(new submit_Listener());//给按钮设置监听事件
 
-            //注册监听短信的广播
-            this.mySMSReceiver = new MySMSReceiver();
-            IntentFilter intentFilter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
-            intentFilter.setPriority(999999999);
-            registerReceiver(this.mySMSReceiver, intentFilter);
+            SmsContent content = new SmsContent(new Handler());
+            //注册短信变化监听
+            this.getContentResolver().registerContentObserver(Uri.parse("content://sms"), true, content);
+
+
         }catch (Exception e){
             Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
         }
@@ -49,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
             try {
                 if(state == 0 ){
                     msg.setText("正在获取闪讯密码，请稍等......");
+                    submit.setText("获取中...");
                     String phoneNumber="1065930051";
                     String message="MM";
                     // 获取短信管理器
@@ -60,9 +62,9 @@ public class MainActivity extends AppCompatActivity {
                     }
                     state=1;//表示短信发送成功
                 }else if(state == 1 ){
-                    Toast.makeText(MainActivity.this, "正在获取中，请稍等...", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "正在获取中，请稍等...", Toast.LENGTH_SHORT).show();
                 }else if(state == 2 ){
-                    Toast.makeText(MainActivity.this, "闪讯密码已经成功获取！", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "闪讯密码已经成功获取！", Toast.LENGTH_SHORT).show();
                 }
             }catch (Exception e){
                 Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
@@ -71,30 +73,30 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-    //短信监听类
-    class MySMSReceiver extends BroadcastReceiver {
-        public void onReceive(Context context,Intent intent){
-            try{
-                Bundle bundle = intent.getExtras();
-                if(bundle != null){
-                    Object [] objArray = (Object []) bundle.get("pdus");
-                    SmsMessage[] messages = new SmsMessage[objArray.length];
-
-                    String body = "";
-                    for (int i = 0; i< objArray.length; i++){
-                        messages[i] = SmsMessage.createFromPdu((byte[]) objArray[i]);
-                        body += messages[i].getDisplayMessageBody();
-                    }
-                    String phoneNumber = messages[0].getDisplayOriginatingAddress();
-                    msg.setText(bodyFormat(body,phoneNumber));
-                    abortBroadcast();//中断广播的传递
-                    unregisterReceiver(mySMSReceiver);//注销广播
+    //监听短信数据库变化
+    class SmsContent extends ContentObserver {
+        private Cursor cursor = null;
+        public SmsContent(Handler handler) {
+            super(handler);
+        }
+        public void onChange(boolean selfChange) {
+            super.onChange(selfChange);
+            ContentResolver resolver=getContentResolver();
+            Uri uri = Uri.parse("content://sms/inbox");
+            Cursor cursor = resolver.query(uri, null, null, null,"date desc");
+            if(cursor.moveToNext()){
+                String address = cursor.getString(cursor.getColumnIndex("address"));
+                long date = cursor.getLong(cursor.getColumnIndex("date"));
+                String body = cursor.getString(cursor.getColumnIndex("body"));
+                if("106593005".equals(address)){
+                    msg.setText(bodyFormat(body,address));
+                    submit.setText("获取成功 !");
+                    state=2;
+                    resolver.delete(Uri.parse("content://sms"), "date=" + date, null);
+                    resolver.unregisterContentObserver(this);
                 }
-            }catch (Exception e){
-                Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
-                msg.setText("获取失败，请重新尝试。");
-                state=0;//表示短信发送失败
             }
+            cursor.close();
         }
         public String bodyFormat(String body,String phone){
             String msg = "";
@@ -102,19 +104,18 @@ public class MainActivity extends AppCompatActivity {
             String deadline = "";
             if(body.length()!=51){
                 state = 0;//接收短信失败
-                return "获取失败，请重新尝试。";
+                return "获取失败，请重新尝试!";
             }
-            if(phone!="106593005"){
+            if("106593005".equals(phone)==false){
                 state = 0;//接收短信失败
                 return "获取失败，请重新尝试";
             }
             pwd=body.substring(18,24);
-            deadline=body.substring(28,46);
-            msg = "密 码："+pwd+"\n"+"截止日期："+deadline;
+            deadline=body.substring(28,47);
+            msg = "闪讯密码："+pwd+"\n\n"+"截止日期："+deadline+"\n";
             return msg;
         }
     }
-
 
 
 }
